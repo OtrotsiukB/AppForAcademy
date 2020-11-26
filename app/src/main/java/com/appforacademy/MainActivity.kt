3c3eb6e6017da52495af0393f194d01ff43d8343
Grechotg <godster900@gmail.com>
2020-11-26 14:01:00 +0200

    Сделал фрагменты (нарисовал)

--- app/src/main/java/com/appforacademy/MainActivity.kt
+++ app/src/main/java/com/appforacademy/MainActivity.kt
@@ -6,11 +6,25 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private var fragment_movies_detal = fragment_movies_details()
+    private var fragment_movies_list = fragment_movies_list()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this,MovieDetailsActivity::class.java)
-        startActivity(intent)
+        //val intent = Intent(this,MovieDetailsActivity::class.java)
+       // startActivity(intent)
+      /*  supportFragmentManager.beginTransaction().apply {
+            add(R.id.Fragment_container_Main,fragment_movies_detal)
+            commit()
+
+        }
+        */
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.Fragment_container_Main,fragment_movies_list)
+            commit()
+
+        }
+
     }
 }
