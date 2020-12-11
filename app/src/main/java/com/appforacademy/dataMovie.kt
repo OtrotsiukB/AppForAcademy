8f502b4a1d997e113e5c81fc862329942cb57392
Grechotg <godster900@gmail.com>
2020-12-11 08:11:42 +0200

    проба

---
+++ app/src/main/java/com/appforacademy/dataMovie.kt
@@ -0,0 +1,6 @@
+package com.appforacademy
+
+data class dataMovie(val NameMovies:String,val IC_movies:Int, val Age:String,
+                     val Genre:String,val Favorit:Boolean, val Like:Int, val Reviews:String,
+                     val Time:String) {
+}
